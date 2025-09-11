6f3c4ea31760e0b723a66615226b15938d1d24fa
Adil Ahmed <adil.ahmed@triptease.com>
2025-09-11 09:57:51 +0100

    Revert "Change package name + publish to the public NPM registry"
    This reverts commit 76f77a39b4b4d546e9995dda0009d9fb1f02d935.

--- release.sh
+++ release.sh
@@ -7,6 +7,8 @@ rm -rf dist && \
 cp -r src/main dist && \
 cp package.json tsconfig.json README.md dist && \
 pushd dist && \
-npm publish --access public && \
+npm config set registry https://us-npm.pkg.dev/triptease-paid-search/tt-paid-search/ && \
+yarn artifactregistry-login && \
+npm publish && \
 popd && \
 rm -r dist
